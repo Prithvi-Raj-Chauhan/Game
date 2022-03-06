b40e79b768ac826e34af0cc9727da741743394e9
Divyanshu Padhy <divyadollypadhy@outlook.com>
2022-03-06 19:57:16 +0530

    added react and respons methods

--- utils/player.py
+++ utils/player.py
@@ -1,10 +1,19 @@
+from this import d
 from utils.mini_games.RandomNumberGuesser import RandomNumberGuesser
 
 class Player:
     def __init__(self, name: str, bal: int) -> None:
         self.name = name
         self.bal = bal
+        self.msg = ""
     
+    def react(self, msg: str):
+        self.msg = msg
+
+    @property
+    def response(self):
+        return self.msg
+
     def addMoney(self, amount: int):
         self.bal += amount
     
