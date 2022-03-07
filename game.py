6c2782e073cb0cbaa4622d685f6adea48dc24b61
Divyanshu Padhy <divyadollypadhy@outlook.com>
2022-03-07 16:44:25 +0530

    added command and handle methods, and even profile viewing functions

--- game.py
+++ game.py
@@ -1,2 +1,9 @@
 import random
 from utils.player import Player
+
+if __name__ == '__main__':
+    player = Player('Prithvi Raj Chauhan', 5000) 
+    while True:
+        player.cmd("Enter the command: ")
+        print(player.response)
+        player.update()
--- utils/player.py
+++ utils/player.py
@@ -1,10 +1,19 @@
 from utils.mini_games.RandomNumberGuesser import RandomNumberGuesser
 
+
 class Player:
     def __init__(self, name: str, bal: int) -> None:
         self.name = name
         self.bal = bal
         self.msg = ""
+
+    def cmd(self, prompt):
+        inp = input(prompt)
+        self.handle(inp)
+
+    def handle(self, inp: str):
+        if inp == 'profile': self.react(profile(self))
+        elif inp == 'exit': self.exit()
     
     def react(self, msg: str):
         self.msg = msg
@@ -22,3 +31,22 @@ class Player:
             return True
         else:
             return False
+
+    def generateProfile(self) -> dict:
+        return {
+            'name': self.name,
+            'bal': self.bal
+        }
+
+    def exit(self):
+        import time
+        print("Exiting in 3 seconds...")
+        time.sleep(3)
+        exit()
+
+    def update(self):
+        self.msg = str()
+
+def profile(p: Player):
+    profile = p.generateProfile()
+    return f"\n\tName: \t{profile['name']}\n\tBalance: \t{profile['bal']}\n"
