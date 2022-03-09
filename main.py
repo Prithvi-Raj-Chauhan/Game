0b840c74f83d2030e78d9ec0b16a72d8132b29eb
Divyanshu Padhy <divyadollypadhy@outlook.com>
2022-03-09 13:34:09 +0530

    added db.json to root dir

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.player import Player
 
 if __name__ == '__main__':
-    player = Player('./database/db.json') 
+    player = Player('db.json') 
     while True:
         player.cmd("Enter the command: ")
         print(player.response)
