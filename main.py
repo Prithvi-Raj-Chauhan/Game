601544a9ebb2003465d091b2b2a5115fdee94b7a
Divyanshu Padhy <divyadollypadhy@outlook.com>
2022-03-17 14:20:03 +0530

    added logic of items from db.json

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from json import dumps
+from json import dumps, load
 from utils.player import Player # the Player class
-from utils.shop import Shop, ITEMS # the shop class and items
+from utils.shop import Item, Shop # the shop class and items
 from os import listdir # for making db.json if non-existent
+
+items_dict = load(open('./db.json'))['shop']
+items = {
+    key: Item(key, value['price'], value['reward']) for key, value in items_dict.items()
+}
+print(items)
 
 def init_db():
     name = input("Enter you name: ")
     with open('db.json', 'w') as f:
         db = {
                 "profile": { "name": name, "bal": 0, "inventory": [] },
-                "shop": { "item": { "price": 0, "type": "" } }
+                "shop": { "item": { "price": 0, "reward": "" } }
             }
         f.write(dumps(db))
     print("Finished intialising !")
@@ -17,7 +23,7 @@ if __name__ == '__main__':
     if 'db.json' not in listdir():
         print("\n===Intialising for first time===")
         init_db()
-    shop = Shop(ITEMS, 'db.json')
+    shop = Shop(items, 'db.json')
     player = Player('db.json', shop) 
     while True:
         cmd = input("Enter the command: ")
